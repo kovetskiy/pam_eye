eb9da9cd03d1fd2816575371502b73d89d46f33b
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-08-11 21:06:14 +0300

    debian pkg

---
+++ debian/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e -u
+
+if [ $# -eq 0 ]; then
+    echo -e "Usage:\n$0 <version>"
+    exit 1
+fi
+
+VERSION="$1"
+PKGDIR="pam_eye-deb"
+SRCDIR="src"
+
+sed -i 's/\$VERSION\$/'$VERSION'/g' $PKGDIR/DEBIAN/control
+
+git clone https://github.com/kovetskiy/pam_eye $SRCDIR
+
+cp -f $SRCDIR/pam_eye $PKGDIR/usr/bin/
+
+dpkg -b $PKGDIR pam_eye-${VERSION}_amd64.deb
+
+# restore version placeholder
+git checkout $PKGDIR
