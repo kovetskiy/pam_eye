5f0871762b087c134a03b1320f460f6a042a4832
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-08-11 21:28:04 +0300

    fix indentation

--- pam_eye.c
+++ pam_eye.c
@@ -12,7 +12,7 @@
 PAM_EXTERN int pam_sm_setcred(
     pam_handle_t *pamh, int flags, int argc, const char **argv
 ) {
-	return PAM_SUCCESS ;
+    return PAM_SUCCESS ;
 }
 
 PAM_EXTERN int pam_sm_authenticate(
